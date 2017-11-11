f56b32e99148111c969fd537983498e8e9c94b20
t1t1 <cha@hanmail2.net>
2017-11-11 11:57:23 +0900

    Added todos

---
+++ src/data/todos.ts
@@ -0,0 +1,20 @@
+export default [
+    {
+        id: 1,
+        title: 'work',
+        description: 'work hard',
+        complete: false
+    },
+    {
+        id: 2,
+        title: 'work2',
+        description: 'work hard2',
+        complete: false
+    },
+    {
+        id: 3,
+        title: 'work3',
+        description: 'work hard3',
+        complete: false
+    }
+]
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
+// import { Component, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import todos from '../../data/todos';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+// export class HomePage implements onInit {
 export class HomePage {
+  todos: {
+    id: number,
+    title: string,
+    description: string,
+    complete: boolean
+  }[];
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ngOnInit() {
+    this.todos = todos;
   }
 
 }
